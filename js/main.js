7c74ea10b3f9e3903a2cd25aecd484358ce4b1ba
Erik <ecordoba@hawk.iit.edu>
2022-03-24 20:05:47 -0500

    add attempted fixes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
+'use strict';
 
+var shippingaddress =  document.getElementById('shippingaddress').value;
+ var shippingzip = document.getElementById('shippingzip').value;
+ var shippingcity =  document.getElementById('shippingcity').value;
+  var shippingstate = document.getElementById('shippingstate').value;
 
 function billingAutofill(){
 		if (document.getElementById('autofill').checked){
-     var address =  document.getElementById('address').value;
-      var zip = document.getElementById('zip').value;
-      var city =  document.getElementById('city').value;
-       var state = document.getElementById('state').value;
 
-     document.getElementById('billingaddress').value = address;
-      document.getElementById('billingzip').value = zip;
-      document.getElementById('billingcity').value = city;
-      document.getElementById('billingstate').value = state;
+     document.getElementById('billingaddress').value = shippingaddress;
+      document.getElementById('billingzip').value = shippingzip;
+      document.getElementById('billingcity').value = shippingcity;
+      document.getElementById('billingstate').value = shippingstate;
 
 		}
 		else{
